d41e9b962e26d13654f98712ef62d837bd3b9626
Fariba Tokhi <38998610+Fariba-Tokhi@users.noreply.github.com>
2026-07-31 18:09:13 -0700

    Clean up comments in .eleventy.js
    Removed comments for clarity in configuration.

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,11 @@
 module.exports = function(eleventyConfig) {
-  // Copy assets
   eleventyConfig.addPassthroughCopy("src/assets/css");
   eleventyConfig.addPassthroughCopy("src/assets/js");
   
-  // Collections
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/projects/*.md");
   });
   
-  // Generate sitemap
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
   
   return {
